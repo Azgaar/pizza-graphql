17a5078b1edb26f52630aa45a53dcbdb0f5a5249
Azgaar <maxganiev@yandex.ru>
2021-11-13 15:18:49 +0300

    add apollo client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,21 @@
 import {StrictMode} from "react";
 import ReactDOM from "react-dom";
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./global.css";
 
+const client = new ApolloClient({
+  uri: "http://localhost:3001/graphql",
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ApolloProvider client={client}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ApolloProvider>,
   document.getElementById("root")
 );
 
